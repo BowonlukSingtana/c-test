04ab7c81e234ad7e61a11d02fe82136908a7515c
Nat <Bowonluksingtana@gmail.com>
2022-10-19 23:08:20 +0700

    c++ test

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  cplusplusTest
+//
+//  Created by nat on 15/10/2565 BE.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int jjj;
+    std::cout << "Help me: \n";
+    std::cin >>jjj;
+    std::cout << "Hello, World! "<< jjj << std::endl;
+    return 0;
+}
